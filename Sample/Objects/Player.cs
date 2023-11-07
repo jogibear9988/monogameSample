1c3b65718bbdba2ac277ea3d3541b2694ec10f2a
jkuehner <jochen.kuehner@kardex.com>
2023-11-07 09:24:09 +0100

    simple sample for monogame

---
+++ Sample/Objects/Player.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sample.Objects
+{
+    public class Player
+    {
+        public void Update(GameTime gameTime)
+        { }
+
+        public void Draw(GameTime gameTime)
+        { }
+    }
+}
